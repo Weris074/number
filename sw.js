4cf2892319f7d9a9ad706a9ae9aac6b21b60161e
Weris074 <Verislav18@gmail.com>
2026-03-15 15:04:55 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'guess-number-v18';
+const ASSETS = ['.', './index.html', './manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
